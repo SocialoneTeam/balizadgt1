f042a073d9d9adabd0c0164be6bb82b02eec775d
Socialone Studio <team@socialone.cat>
2025-11-17 21:01:34 +0100

    Updated package.json

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'react-app',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
